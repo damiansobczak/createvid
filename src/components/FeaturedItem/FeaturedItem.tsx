67babae34513da04ff4fca2dbafad1f2401cd5cf
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2020-10-11 11:37:37 +0200

    FeaturedItem styling improvements

--- src/components/FeaturedItem/FeaturedItem.tsx
+++ src/components/FeaturedItem/FeaturedItem.tsx
@@ -33,13 +33,28 @@ export default function FeaturedItem(props: IFeaturedItem) {
   return (
     <div className="featured-item" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       {media === Media.thumbnail ? (
-        <img className="featured-item__media" src={props["im:image"][2].label} alt="" />
+        <img className="featured-item__media" src={props["im:image"][2].label} alt={props["im:name"].label} />
       ) : (
         <video className="featured-item__media" width="100" height="100" ref={el}>
           <source src={props.link[1].attributes.href} type={props.link[1].attributes.type} />
           Your browser does not support the video tag.
         </video>
       )}
+      <div className="featured-item__header">
+        <button className="featured-item__action">
+          <span className="icon-thumbs-up"></span>
+        </button>
+        <button className="featured-item__action">
+          <span className="icon-thumbs-down"></span>
+        </button>
+        {props["im:rentalPrice"]?.label && <div className="featured-item__price">{props["im:rentalPrice"].label}</div>}
+      </div>
+      <div className="featured-item__footer">
+        <div className="featured-item__title">{props["im:name"].label}</div>
+        <div className="featured-item__info">
+          {props.category.attributes.label} / {props["im:releaseDate"].attributes.label}
+        </div>
+      </div>
     </div>
   );
 }
